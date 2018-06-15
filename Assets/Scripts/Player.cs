280d78614ab9470b4621c5cdd598bf96f802b33a
Alex Makepeace <alexm@beamdog.com>
2018-06-15 10:20:50 -0600

    Switching back to default c# linter, linting Player again

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
+    [Header("Movement")]
     [HideInInspector] public bool facingRight = true;
     [HideInInspector] public bool jump = false;
     public float moveForce = 365f;
@@ -15,44 +17,50 @@ public class Player : MonoBehaviour {
     private Rigidbody2D rb2d;
 
     // Use this for initialization
-    void Awake () {
+    void Awake()
+    {
         //anim = GetComponent<Animator>();
-        rb2d = GetComponent<Rigidbody2D> ();
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update () {
-        grounded = Physics2D.Linecast (transform.position, groundCheck.position, 1 << LayerMask.NameToLayer ("Ground"));
+    void Update()
+    {
+        grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
 
-        if (Input.GetButtonDown ("Jump") && grounded) {
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
             jump = true;
         }
     }
 
-    void FixedUpdate () {
-        float h = Input.GetAxis ("Horizontal");
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
 
         // anim.SetFloat("Speed", Mathf.Abs(h));
 
         if (h * rb2d.velocity.x < maxSpeed)
-            rb2d.AddForce (Vector2.right * h * moveForce);
+            rb2d.AddForce(Vector2.right * h * moveForce);
 
-        if (Mathf.Abs (rb2d.velocity.x) > maxSpeed)
-            rb2d.velocity = new Vector2 (Mathf.Sign (rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);
+        if (Mathf.Abs(rb2d.velocity.x) > maxSpeed)
+            rb2d.velocity = new Vector2(Mathf.Sign(rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);
 
         if (h > 0 && !facingRight)
-            Flip ();
+            Flip();
         else if (h < 0 && facingRight)
-            Flip ();
+            Flip();
 
-        if (jump) {
+        if (jump)
+        {
             //  anim.SetTrigger("Jump");
-            rb2d.AddForce (new Vector2 (0f, jumpForce));
+            rb2d.AddForce(new Vector2(0f, jumpForce));
             jump = false;
         }
     }
 
-    void Flip () {
+    void Flip()
+    {
         facingRight = !facingRight;
         Vector3 theScale = transform.localScale;
         theScale.x *= -1;
