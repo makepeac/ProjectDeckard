797b96313152c313c6872a1548c79f046987074c
Alex Makepeace <alexm@beamdog.com>
2018-08-03 15:22:59 -0600

    Spawning NPCs at appropriate points

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField]
+    RectTransform npcStartingLocation;
+
+    [SerializeField]
+    List<NPC> npcs;
+
+    public void UpdateNpcs()
+    {
+        foreach (NPC npc in npcs)
+        {
+            Transform spawnPoint = npc.getCurrentSpawnPoint();
+            if (spawnPoint)
+            {
+                Debug.Log(spawnPoint.position);
+                npc.transform.position = new Vector3(spawnPoint.position.x, spawnPoint.position.y);
+            }
+            else
+            {
+                npc.transform.position = new Vector3(npcStartingLocation.position.x, npcStartingLocation.position.y);
+            }
+
+        }
+    }
+}
