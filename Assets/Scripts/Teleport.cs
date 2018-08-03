f471f9dd91355263557b9d573188511f0245296c
Alex Makepeace <alexm@beamdog.com>
2018-08-03 11:46:00 -0600

    Add a room transitions prefab and teleportation script

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    [SerializeField]
+    Transform destination = null;
+
+    /// <summary>
+    /// Sent when another object enters a trigger collider attached to this
+    /// object (2D physics only).
+    /// </summary>
+    /// <param name="other">The other Collider2D involved in this collision.</param>
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            other.gameObject.transform.position = new Vector3(destination.position.x, destination.position.y);
+        }
+    }
+}
